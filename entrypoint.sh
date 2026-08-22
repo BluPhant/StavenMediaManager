e271eb328e4eebfed5bcc00d9481c43ddcf10e57
Blue Phantom <bluphant@gmail.com>
2026-08-22 10:52:14 -0400

    entrypoint: fix sh substring syntax (use cut instead of bash ${var:0:7})

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,6 @@
 # Substitute build-time placeholders in static HTML so browsers pick up new JS/CSS
 # on each new image version without needing hard-reloads.
 REV="${APP_REVISION:-dev}"
-SHORT="${REV:0:7}"
+SHORT=$(echo "$REV" | cut -c1-7)
 sed -i "s/__APP_VERSION__/${SHORT}/g" /app/static/index.html
 exec uvicorn app.main:app --host 0.0.0.0 --port 8080
